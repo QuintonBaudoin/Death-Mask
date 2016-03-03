a6549cf6392bba27fc8a9259845cecb6ff4509fd
Quinton Baudoin <baudoin.q@gmail.com>
2016-03-02 18:08:02 -0600

    started input handler

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+    
+    public float speed = 5.0f;
+    KeyCode Left = KeyCode.A;
+    KeyCode Right = KeyCode.D;
+    KeyCode Up = KeyCode.W;
+    KeyCode Down = KeyCode.D;
+
+    KeyCode Jump = KeyCode.Space;
+    bool canJump = true;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(Left))
+        {
+            pos.x-=speed*Time.deltaTime;
+        }
+        if (Input.GetKey(Right))
+        {
+            pos.x+=speed * Time.deltaTime;
+        }
+        if (Input.GetKey(Up))
+        {
+           //do shit
+        }
+        if (Input.GetKey(Down))
+        {
+            //do shit
+        }
+        if (Input.GetKey(Jump))
+        {
+            //
+        }
+
+        transform.position = pos;
+    }
+
+    void OnCollision(Collision c)
+    {
+       
+    }
+
+}
