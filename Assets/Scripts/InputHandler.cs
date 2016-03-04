c79b9f632fb53eb679fd02c100b031b668113832
Quinton Baudoin <baudoin.q@gmail.com>
2016-03-04 16:11:18 -0600

    Created a Crude input script

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,33 +4,81 @@ using System.Collections;
 public class InputHandler : Singleton<MonoBehaviour>
 {
 
-    
+    [SerializeField] GameObject WeaponA;
+
 
     public float speed = 5.0f;
     public float jumpSpeed = 5.0f;
     public float jumpHeight = 5.0f;
+    Vector3 direction;
 
-    int currentNode;
+    [SerializeField] KeyCode Left = KeyCode.A;
+    [SerializeField] KeyCode Right = KeyCode.D;
+    [SerializeField] KeyCode Up = KeyCode.W;
+    [SerializeField] KeyCode Down = KeyCode.S;
 
+    [SerializeField] KeyCode K = KeyCode.K;
 
-    KeyCode Left = KeyCode.A;
-    KeyCode Right = KeyCode.D;
-    KeyCode Up = KeyCode.W;
-    KeyCode Down = KeyCode.S;
+    [SerializeField] KeyCode Jump = KeyCode.Space;
+    bool onGround = true;
 
-    KeyCode Jump = KeyCode.Space;
-    bool jumping = true;
-
+    Rigidbody m_Rigid;
     
+    void Start()
+    {
+        m_Rigid = gameObject.GetComponent<Rigidbody>();
+
+        m_Rigid.constraints = RigidbodyConstraints.FreezeRotation;
+    }
     void Update()
     {
-        if(Input.GetKey(Right))
+        Vector3 velocity = gameObject.GetComponent<Rigidbody>().velocity;
+
+        if (onGround)
         {
-          LevelPath.GetNodes();  
+            if (Input.GetKey(Right))
+            {
+                gameObject.transform.forward = new Vector3(0, 0, 1);
+                velocity.x = speed ;
+            }
+
+            else if (Input.GetKey(Left))
+            {
+                gameObject.transform.forward = new Vector3(0, 0, -1);
+               velocity.x = -speed ;
+            }
+            else velocity.x = 0;
+            if(Input.GetKey(Jump))
+            {
+                
+                velocity.y = jumpHeight;
+            }
+
+            if (Input.GetKey(K))
+                Attack(true);
+            else
+            {
+                Attack(false);
+            }
         }
 
 
+
+
+        gameObject.GetComponent<Rigidbody>().velocity = velocity;
     }
 
+
+    void Attack(bool state)
+    {
+   
+      if(WeaponA.GetComponent<DamagingObject>())
+        {
+            WeaponA.GetComponent<DamagingObject>().active = state;
+        }
+
+
+
+    }
 
 }
