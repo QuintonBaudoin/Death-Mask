58e9020e05cde88bf05d08d6bca84eba708b1058
slanden <slanden523@gmail.com>
2016-03-04 11:52:55 -0600

    Interaction/Dialog
    -basic functionality of interacting with NPCs
    -first pass at dialog

---
+++ Assets/Scripts/MoveTemporary.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTemporary : MonoBehaviour
+{
+    float speed = 0.1f;    
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.position += new Vector3(1, 0, 0) * speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(-1, 0, 0) * speed;
+        if (Input.GetKey(KeyCode.W))
+            transform.position += new Vector3(0, 0, -1) * speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += new Vector3(0, 0, 1) * speed;
+
+    }
+
+
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour, InteractInterface
+{
+    char interactKey = 'E';
+    string text = "Howdy";
+    string longText = "Good day fellow capsule, what brings you to this fine piece of unfinished structure? That's okay good sir, I can see you do not have a mouth so there's no need to answer that. I also do not have a mouth so I am infact talking to myself...in my head.";
+    int textOffset = 20;
+
+    bool triggered = false;
+    bool talking = false;
+
+    public void Interact()
+    {
+        //position of the character
+        Vector3 charPos = transform.position;
+        //apply offset for text position
+        charPos.y += textOffset;        
+        //convert to screen space
+        charPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        GUI.Label(new Rect(charPos.x, charPos.y, 60, 30), text);
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        triggered = true;
+
+        if (c.gameObject.name == "Player")
+        {
+            
+        }
+        
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        triggered = false;
+        talking = false;
+    }
+
+    void OnGUI()
+    {
+        if (triggered)
+        {
+            //size of textbox
+            Vector2 labelSize;
+            //position of the character
+            Vector3 charPos = transform.position;
+            //apply offset for text position
+            //charPos.y += transform.localScale.y / 2 /*+ textOffset*/;
+            //convert to screen space
+            charPos = Camera.main.WorldToScreenPoint(charPos);
+
+            if (!talking)
+            {
+                //calculate the size of text box for the given text
+                labelSize = GUIStyle.none.CalcSize(new GUIContent("Press " + interactKey + " to Interact"));
+                labelSize.y += GUIStyle.none.CalcHeight(new GUIContent("Press " + interactKey + " to Interact"), labelSize.x);
+
+                GUI.Label(new Rect(charPos.x - labelSize.x / 2, charPos.y, labelSize.x, labelSize.y), "Press " + interactKey + " to Interact");
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    talking = true;
+                }
+            }
+            else if (talking)
+            {
+                labelSize = GUIStyle.none.CalcSize(new GUIContent(longText));
+                labelSize.y += GUIStyle.none.CalcHeight(new GUIContent(longText), labelSize.x);
+                if (labelSize.x > 200)
+                {
+                    labelSize.x /= 3;
+                    labelSize.y *= 3;
+                }
+
+                GUI.Label(new Rect(charPos.x - labelSize.x / 2, charPos.y - (labelSize.y + textOffset), labelSize.x, labelSize.y), longText);
+            }
+        }
+    }
+
+}
