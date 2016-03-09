593c888d12bc83a9bb2bc0389a4b5002539ca003
Zac-King <Zacharyking18@yahoo.com>
2016-03-09 16:55:42 -0600

    Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enemy : MonoBehaviour, IHealth
+{
+    [SerializeField]
+    private int Health;
+
+    public void ModifyHealth(int damage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int ReturnHealth()
+    {
+        return Health;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider co)
+    {
+        IHealth test = co.gameObject.GetComponent<IHealth>();
+        if (co.gameObject.tag == "Player" && test != null )
+        {
+            test.ModifyHealth(1);
+        }
+    }
+}
