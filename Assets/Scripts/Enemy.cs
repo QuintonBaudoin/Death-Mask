817d846d9935727aef0879df1d21a005dcb6e48a
Zac-King <Zacharyking18@yahoo.com>
2016-03-09 18:14:45 -0600

    IDamageable and Animator work

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,32 +2,81 @@
 using System.Collections;
 using System;
 
-public class Enemy : MonoBehaviour, IHealth
+public class Enemy : MonoBehaviour, IDamageable
 {
     [SerializeField]
-    private int Health;
+    private int health;
+    [SerializeField]
+    private int maxHealth;
+    [SerializeField]
+    private bool alive;
 
-    public void ModifyHealth(int damage)
+    GameObject parent;
+    Animator anim;
+
+    void Start()
     {
-        throw new NotImplementedException();
+        parent = transform.parent.gameObject;
+        anim = GetComponent<Animator>();
     }
 
-    public int ReturnHealth()
+    public bool Alive
     {
-        return Health;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-    }
-
-    void OnTriggerEnter(Collider co)
-    {
-        IHealth test = co.gameObject.GetComponent<IHealth>();
-        if (co.gameObject.tag == "Player" && test != null )
+        get
         {
-            test.ModifyHealth(1);
+            return alive;
+        }
+
+        set
+        {
+            alive = value;
+        }
+    }
+
+    public int MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+
+        set
+        {
+            maxHealth = value;
+        }
+    }
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+        }
+    }
+
+    public void OnDeath()
+    {
+        // trigger animation
+        // remove collider
+        // sink into earth
+    }
+
+    [ContextMenu ("Damage")]
+    public void TakeDamage()
+    {
+        // trigger animation
+        Health --;
+        if(Health < 1)
+        {
+            Debug.Log(Health);
+            transform.parent = null;
+            Destroy(parent);
+            anim.SetTrigger("Dead");
         }
     }
 }
