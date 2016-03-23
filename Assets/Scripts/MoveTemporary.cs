313c1d5a22dc67642e91a52684236de840672ba7
slanden <slanden523@gmail.com>
2016-03-23 17:51:00 -0500

    Camera
    -smoothing

--- Assets/Scripts/MoveTemporary.cs
+++ Assets/Scripts/MoveTemporary.cs
@@ -8,13 +8,25 @@ public class MoveTemporary : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
+        {
             transform.position += new Vector3(1, 0, 0) * speed;
+            transform.forward = new Vector3(1, 0, 0);
+        }
         if (Input.GetKey(KeyCode.D))
+        {
             transform.position += new Vector3(-1, 0, 0) * speed;
+            transform.forward = new Vector3(-1, 0, 0);
+        }
         if (Input.GetKey(KeyCode.W))
+        {
             transform.position += new Vector3(0, 0, -1) * speed;
+            transform.forward = new Vector3(0, 0, -1);
+        }
         if (Input.GetKey(KeyCode.S))
+        {
             transform.position += new Vector3(0, 0, 1) * speed;
+            transform.forward = new Vector3(0, 0, 1);
+        }
 
     }
 
