fe376d2afd4262af22a3a819ff145d4b5fb05c7c
Zac King <Zacharyking18@yahoo.com>
2016-04-06 18:04:57 -0500

    Start of GUI

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour
+{
+    private Slider HealthBar;
+
+    private static GUIManager instance;
+
+    public static GUIManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            { instance = new GUIManager(); }
+            return instance;
+        }
+    }
+
+    GUIManager() { }
+
+    void Awake()
+    {
+        HealthBar = (GameObject.Find("HealthSlider") as object) as Slider; 
+    }
+
+
+    bool UpdateHealthBar(int min, int max)
+    {
+
+
+        return false;
+    }
+}
