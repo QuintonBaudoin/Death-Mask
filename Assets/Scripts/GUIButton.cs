4531416f4d976dfb13e380a8bc69a7721a213a3d
quintonbaudoin <baudion.q@gmail.com>
2016-05-18 16:08:34 -0500

    added credits scene fixed some small htings

---
+++ Assets/Scripts/GUIButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GUIButton : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void IntroMenu()
+    {
+
+        SceneManager.LoadScene("IntroMenu");
+    }
+
+}
